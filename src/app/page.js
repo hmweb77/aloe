55af8e17e0f51d943053d99af98fcbee1565d5b7
hmweb77 <oussamahamdani1@gmail.com>
2024-05-13 12:04:52 +0300

    adding submit text

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,108 @@
+"use client";
 import Image from "next/image";
-
+import Logo from "@/app/assets/logoPic.png";
+import AllPic from "@/app/assets/Screenshot 2024-05-02 at 11.37.12.png";
+import Rond from "@/app/assets/rond.png";
+import { useState } from "react";
+import axios from "axios";
 export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [language, setLanguage] = useState("");
+  const [statusMessage, setStatusMessage] = useState(""); 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSetName = (e) => setName(e.target.value);
+  const handleSetEmail = (e) => setEmail(e.target.value);
+  const handleSetLanguage = (e) => setLanguage(e.target.value);
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setStatusMessage("Processing your subscription...");
+    try {
+      const response = await axios.post('/api/subscribe', { email,name,language });
+      setIsSubmitting(true);  
+      setStatusMessage("Thank you for subscribing!");
+      setName("");
+      setEmail("");
+      setLanguage("");
+    } catch (error) {
+      console.error('Error submitting form:', error.message);
+      setStatusMessage("Failed to subscribe. Please try again later.");
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-green-100">
+      <div className="flex justify-center md:justify-start">
+        <Image src={Logo} alt="aloevera Logo" width={250} />
+      </div>
+      <div className="flex flex-col md:flex-row justify-center">
+        <div className="flex-1 order-1 md:order-1 ml-2 my-4 md:my-1">
+          <Image src={AllPic} alt="pictures" width={600} />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+        <div className="flex-1 flex flex-col order-2 md:order-2 mx-3 ">
+          <h2 className="text-3xl font-poppins md:text-5xl font-bold text-white mb-4">
+            RECEIVE YOUR GIFT !
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+          <p className="text-xl text-white font-poppins custom-text-stroke mb-6 md:mr-24">
+            Sign up for our newsletter and receive immediately your first gift !
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <form onSubmit={handleSubmit}>
+            <div className="w-full max-w-sm">
+              <input
+                onChange={handleSetName}
+                value={name}
+                className="mb-4 w-full px-4 py-2 text-lg bg-transparent border-b-2 border-black focus:border-white outline-none placeholder-white"
+                type="text"
+                placeholder="Firstname"
+              />
+              <label className=" text-white mb-4 w-full px-4 py-2 text-lg bg-transparent  ">
+                {" "}
+                Select a language
+              </label>
+              <select
+                value={language}
+                onChange={handleSetLanguage}
+                className="text-white  mb-4 w-full px-4 py-2 text-lg bg-transparent border-b-2 border-black focus:border-white outline-none placeholder-white"
+              >
+                <option value="english">English</option>
+                <option value="French">French</option>
+                <option value="Portugues">Portugues</option>
+              </select>
+              <input
+                onChange={handleSetEmail}
+                value={email}
+                className="w-full px-4 py-2 text-lg bg-transparent border-b-2 border-black focus:border-white outline-none placeholder-white"
+                type="email"
+                placeholder="E-mail"
+              />
+            </div>
+            {!isSubmitting ? (
+              <button
+              type="submit"
+              className="mt-6 bg-white text-2xl font-extrabold text-green-100 py-4 px-8 rounded-3xl md:w-96 transition-colors"
+            >
+              SUBSCRIBE
+            </button>
+            ) : (
+              <p
+              className="mt-6  bg-white text-2xl font-extrabold text-green-100 py-4 px-8 rounded-3xl md:w-96 transition-colors"
+            >
+            {statusMessage}
+            </p>
+            )}
+          </form>
+        </div>
+        <div className="flex flex-row-reverse order-3 md:order-3 ">
+          <Image
+            src={Rond}
+            alt="product"
+            priority
+            className="border-none md:mt-24"
+          />
+        </div>
       </div>
     </main>
   );
